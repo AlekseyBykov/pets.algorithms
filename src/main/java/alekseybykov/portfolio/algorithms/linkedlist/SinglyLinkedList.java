40728d9989f7ec58406107343f0ec0b5dd533dfa
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-04-20 06:26:01 +0400

    Added example of the singly linked list.

---
+++ src/main/java/alekseybykov/portfolio/algorithms/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,101 @@
+package alekseybykov.portfolio.algorithms.linkedlist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SinglyLinkedList {
+
+	private Node head;
+	private class Node {
+		int data;
+		Node nextNode;
+
+		Node(int d) {
+			data = d;
+			nextNode = null;
+		}
+	}
+
+	public SinglyLinkedList(int[] data) {
+		for (int i = 0; i < data.length; i++) {
+			addElementAtEnd(data[i]);
+		}
+	}
+
+	private void addElementAtEnd(int data) {
+		Node node;
+		Node temp = new Node(data);
+		if (head == null) {
+			head = temp;
+			return;
+		}
+
+		node = head;
+		while (node.nextNode != null) {
+			node = node.nextNode;
+		}
+		node.nextNode = temp;
+	}
+
+	public List<Integer> getAllNodesData() {
+		Node node;
+		if (head == null) {
+			return Collections.emptyList();
+		}
+
+		List<Integer> list = new ArrayList<>();
+
+		node = head;
+		while (node != null) {
+			list.add(node.data);
+			node = node.nextNode;
+		}
+		return list;
+	}
+
+	public int getNumberOfNodes() {
+		int counter = 0;
+		Node node = head;
+		while (node != null) {
+			counter++;
+			node = node.nextNode;
+		}
+
+		return counter;
+	}
+
+	public boolean isElementExists(Integer data) {
+		if (data == null) {
+			return false;
+		}
+
+		Node node = head;
+		while (node != null) {
+			if (data == node.data) {
+				return true;
+			}
+			node = node.nextNode;
+		}
+
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/alekseybykov/portfolio/algorithms/linkedlist/SinglyLinkedListTest.java
@@ -0,0 +1,16 @@
+package alekseybykov.portfolio.algorithms.linkedlist;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SinglyLinkedListTest {
+
+	@Test
+	public void testGetNumberOfNodes() {
+		int[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		SinglyLinkedList singlyLinkedList = new SinglyLinkedList(data);
+		assertThat(singlyLinkedList.getNumberOfNodes(), is(10));
+	}
+}
